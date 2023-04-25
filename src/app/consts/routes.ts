1d414b2735cb939c83640181fffec8bc70005f4d
Laka Nodia <112949971+Lakanno@users.noreply.github.com>
2023-04-25 10:46:10 +0400

    Create auth module and components

--- src/app/consts/routes.ts
+++ src/app/consts/routes.ts
@@ -1,11 +1,11 @@
 export enum routes {
     DASHBOARD = '/dashboard',
     PROFILE = '/profile',
-    
+
     COMMERCE_MANAGE= '/commerce/manage',
     COMMERCE_GRID = '/commerce/grid',
     COMMERCE_PAGE = '/commerce/page',
-
+    
     USER_LIST = '/user/list',
     USER_ADD = '/user/add',
     USER_EDIT = '/user/edit',

2fd0ad125a5e5eb64dbc87e2f433e801a53376e3
Laka Nodia <112949971+Lakanno@users.noreply.github.com>
2023-04-26 11:24:44 +0400

    Add auth guars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './pages/auth/guards/auth.guard';
 import { NotFoundComponent } from './pages/not-found/not-found/not-found.component';
 import { ProfilePageComponent } from './pages/profile/containers/profile-page/profile-page.component';
 
 const routes: Routes = [ 
   {
     path: '',
-    redirectTo: '/auth', pathMatch: 'full'
+    redirectTo: '/auth', 
+    pathMatch: 'full'
   },
 
   {
     path: 'dashboard',
     pathMatch: 'full',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./pages/dashboard/dashboard.module').then(
         (m) => m.DashboardModule
@@ -21,6 +24,7 @@ const routes: Routes = [
   {
     path: 'profile',
     pathMatch: 'full',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./pages/profile/profile.module').then(
         (m) => m.ProfileModule
@@ -28,11 +32,13 @@ const routes: Routes = [
   },
   {
     path: 'commerce',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./pages/commerce/commerce.module').then(m => m.CommerceModule)
   },
 
   {
     path: 'user',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule)
   },
 
